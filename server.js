d3f2aaea48d5c236fe3e3f576dddeeca2e041b65
Ashur213 <ashur7@live.se>
2020-02-07 10:14:26 +0100

    Fortsättning med authen

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express")
 const app = express()
+const bcryptjs = require('bcryptjs') 
 
-const users = [express.json()]
+app.use(express.json())
+
+var users = []
 
 app.get("/users", (req, res) => {
-    res.json(users)
+    res.json(users).send()
 })
 
-app.post ("/users", (req, res) => {
-    const user = { name: req.body.name, password: req.body.password  }
-    users.push(user)
-    res.status(201).send()
+app.post ("/users", async (req, res) => {
+
+    try{
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcryptjs.hash(req.body.password, salt)
+        console.log(salt)
+        console.log(hashedPassword)
+        const user = { name: req.body.name, password: hashedPassword  }
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send
+    }
+
 })
   
 app.listen(3000)
