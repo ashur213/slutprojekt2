0f04035d59654339228c833e463fdb76d42cdb49
Ashur213 <ashur7@live.se>
2020-02-18 11:58:59 +0100

    Lite front end och user authentication

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const bcryptjs = require('bcryptjs') 
+const bcryptjs = require('bcryptjs')
 
 app.use(express.json())
 
@@ -10,20 +10,33 @@ app.get("/users", (req, res) => {
     res.json(users).send()
 })
 
-app.post ("/users", async (req, res) => {
+app.post("/users", async (req, res) => {
 
-    try{
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcryptjs.hash(req.body.password, salt)
-        console.log(salt)
-        console.log(hashedPassword)
-        const user = { name: req.body.name, password: hashedPassword  }
+    try {
+        const hashedPassword = await bcryptjs.hash(req.body.password, 10)
+        const user = { name: req.body.name, password: hashedPassword }
         users.push(user)
         res.status(201).send()
     } catch {
-        res.status(500).send
+        res.status(500).send()
     }
 
 })
-  
+
+app.post("/users/login", async (req, res) => {
+    const user = users.find(user => user.name == req.body.name)
+    if (user == null) {
+        return res.status(400).send("cannot find user")
+    }
+    try {
+        if (await bcryptjs.compare(req.body.password, user.password)) {
+            res.send("Success")
+        } else {
+            res.send("Not allowed")
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
 app.listen(3000)
