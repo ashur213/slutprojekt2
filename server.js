9519c5ec88d1928180e29e6656973ad610490c32
Ashur213 <ashur7@live.se>
2020-02-19 10:35:47 +0100

    Fixat register form

--- server.js
+++ server.js
@@ -3,40 +3,65 @@ const app = express()
 const bcryptjs = require('bcryptjs')
 
 app.use(express.json())
+app.use(express.urlencoded())
 
-var users = []
-
-app.get("/users", (req, res) => {
-    res.json(users).send()
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "\\index.html")
+})
+app.get('/kontakt', (req, res) => {
+  res.sendFile(__dirname + "\\kontakt.html")
+})
+app.get('/style', (req, res) => {
+  res.sendFile(__dirname + "\\style.css")
+})
+app.get('/tradar', (req, res) => {
+  res.sendFile(__dirname + "\\tradar.html")
+})
+app.get('/topic', (req, res) => {
+  res.sendFile(__dirname + "\\topic.html")
+})
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + "\\index.html")
 })
 
-app.post("/users", async (req, res) => {
+app.post('/register', function (req, res) {
+  console.log(req.body.Name, req.body.Password, req.body.Email)
+  res.sendFile(__dirname + "\\kontakt.html")
 
-    try {
-        const hashedPassword = await bcryptjs.hash(req.body.password, 10)
-        const user = { name: req.body.name, password: hashedPassword }
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
+});
 
-})
+//var users = []
+// app.get("/users", (req, res) => {
+//     res.json(users).send()
+// })
 
-app.post("/users/login", async (req, res) => {
-    const user = users.find(user => user.name == req.body.name)
-    if (user == null) {
-        return res.status(400).send("cannot find user")
-    }
-    try {
-        if (await bcryptjs.compare(req.body.password, user.password)) {
-            res.send("Success")
-        } else {
-            res.send("Not allowed")
-        }
-    } catch {
-        res.status(500).send()
-    }
-})
+// app.post("/users", async (req, res) => {
+
+//     try {
+//         const hashedPassword = await bcryptjs.hash(req.body.password, 10)
+//         const user = { name: req.body.name, password: hashedPassword }
+//         users.push(user)
+//         res.status(201).send()
+//     } catch {
+//         res.status(500).send()
+//     }
+
+// })
+
+// app.post("/users/login", async (req, res) => {
+//     const user = users.find(user => user.name == req.body.name)
+//     if (user == null) {
+//         return res.status(400).send("cannot find user")
+//     }
+//     try {
+//         if (await bcryptjs.compare(req.body.password, user.password)) {
+//             res.send("Success")
+//         } else {
+//             res.send("Not allowed")
+//         }
+//     } catch {
+//         res.status(500).send()
+//     }
+// })
 
 app.listen(3000)
